ca11f6e037958c560aeeca230040b2cbb5b40cca
Meghraj <meghraj.deshmukh103@gmail.com>
2022-03-18 00:11:55 +0530

    useDebounce customHook

--- src/App.js
+++ src/App.js
@@ -4,17 +4,28 @@ import React, { useEffect, useState } from "react";
 import useDebounce from "./DebounceHook";
 export default function App() {
   const [input, setInput] = useState("");
-  const [isAPIfired, isTyping] = useDebounce(input);
+  //console.log("Component  is rendering");
+
+  const [isAPIfired, setIsTyping] = useDebounce(input);
 
   useEffect(() => {
+    if (input) setIsTyping(true);
+  }, [input]);
+
+  useEffect(() => {
+    // console.log("useEffect of component");
+
     console.log(isAPIfired);
     if (isAPIfired) {
       //first make it false
       //call the api
-      isTyping.current = true;
+      // isTyping.current = true;
+      //setIsTyping(false);
+      // console.log(isAPIfired);
+      console.log("call the api please");
     }
   }, [isAPIfired]);
-
+  /// console.log("return of compoent ");
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
--- src/DebounceHook/index.js
+++ src/DebounceHook/index.js
@@ -2,25 +2,32 @@ import React, { useEffect, useRef, useState } from "react";
 
 export default function useDebounce(input) {
   //const [input,setInput]=React.useState(input);
+  //console.log("HOOK is rendering ");
+  const [isTyping, setIsTyping] = useState(false);
   const [intervalId, setIntervalId] = React.useState(null);
   const [apiFired, setApifired] = useState(false);
-  const isTyping = useRef(true);
+
+  //const isTyping = useRef(false);
+
   useEffect(() => {
-    if (input && isTyping) {
-      if (intervalId) {
-        clearInterval(intervalId);
-      }
-      const id = setInterval(() => {
+    //console.log("useEffect of cusotmHooks, intercalid", isTyping);
+
+    clearInterval(intervalId);
+
+    if (input) {
+      const id = setTimeout(() => {
         //fire event,call the api and send response
+        // console.log("into interal , statechanged", isTyping);
         setApifired(true);
-        isTyping.current = false;
+        setIsTyping(false);
       }, 300);
       setIntervalId(id);
     }
+
     if (apiFired) {
       setApifired(false);
     }
   }, [input]);
 
-  return [apiFired, isTyping];
+  return [apiFired, setIsTyping];
 }
