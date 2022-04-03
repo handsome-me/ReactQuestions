b32615e3b84f403e34d0cb2c782a537637666dc2
Meghraj <meghraj.deshmukh103@gmail.com>
2022-04-03 23:40:39 +0530

    Built Testing Logic is built- Testing on different condition

--- src/App.js
+++ src/App.js
@@ -1,42 +1,8 @@
 import "./styles.css";
 import React, { useEffect, useState } from "react";
+import FacnyAutoComplete from "./FancyAutocomplete";
 
-import useDebounce from "./DebounceHook";
 export default function App() {
-  const [input, setInput] = useState("");
-  //console.log("Component  is rendering");
-
-  const [isAPIfired, setIsTyping] = useDebounce(input);
-
-  useEffect(() => {
-    if (input) setIsTyping(true);
-  }, [input]);
-
-  useEffect(() => {
-    // console.log("useEffect of component");
-
-    console.log(isAPIfired);
-    if (isAPIfired) {
-      //first make it false
-      //call the api
-      // isTyping.current = true;
-      //setIsTyping(false);
-      // console.log(isAPIfired);
-      console.log("call the api please");
-    }
-  }, [isAPIfired]);
   /// console.log("return of compoent ");
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <input
-        value={input}
-        type="text"
-        onChange={(event) => {
-          setInput(event.target.value);
-        }}
-      />
-    </div>
-  );
+  return <FacnyAutoComplete />;
 }
--- src/FancyAutocomplete/index.js
+++ src/FancyAutocomplete/index.js
@@ -1,5 +1,69 @@
 import React, { useEffect, useState } from "react";
+import "./index.css";
+const listOfUPI = [
+  "@rbl",
+  "@paytm",
+  "@okicici",
+  "@sbi",
+  "@aubank",
+  "@yesbank",
+  "@hdfcbank",
+  "@alibank",
+  "@upi",
+  "@okaxis",
+  "@indusbank",
+  "@unionbank",
+  "@bankofmaharashtra",
+  "@paytm",
+  "@indusbank",
+  "@rbi"
+];
 
-const FacnyAutoComplete = function (props) {};
+const FacnyAutoComplete = function (props) {
+  const [inputVal, setInputVal] = useState("");
+  const [suggestList, setsuggestList] = useState([]);
 
-return FacnyAutoComplete;
+  const filterListOfUPI = (value) => {
+    //value - finding value
+    if (value[value.length - 1] === "@") {
+      return listOfUPI;
+    }
+
+    const splited = value.split("@");
+    console.log("splited", splited);
+    value = splited[1];
+    const filteredUPI = listOfUPI.filter((val) => val.includes(value));
+    return filteredUPI;
+  };
+  return (
+    <div className="form-container">
+      <input
+        onChange={(event) => {
+          const value = event.target.value;
+          setInputVal(value);
+          //checking of value is not empty string -> @ should not be the first char
+          if (
+            (value && value[value.length - 1] === "@") ||
+            (value.includes("@") && value[0] != "@")
+          ) {
+            const allUPI = filterListOfUPI(value);
+            console.log("filtered upi list ", allUPI);
+            setsuggestList(allUPI);
+          }
+        }}
+        value={inputVal}
+      ></input>
+
+      {suggestList.length && (
+        <div className="form-suggestions-container">
+          {suggestList.length &&
+            suggestList.map((item) => {
+              return <div>{item}</div>;
+            })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FacnyAutoComplete;
